aa996bcfffade08bf7170a54a1461e70df325438
Dmytro <44460494+DmytroTitov@users.noreply.github.com>
2020-01-26 14:17:34 +0200

    For review v1

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const checkForSpam = function(message) {
+  const words = message.toLowerCase();
+  return words.includes('sale') || words.includes('spam');
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
---
+++ js/task-6.js
@@ -0,0 +1,49 @@
+// Вариант 1
+// const valueInputRef = document.querySelector('.js-value');
+// const addValueBtn = document.querySelector('.js-add-value');
+// const calcBtn = document.querySelector('.js-calculate');
+// const numbers = [];
+// let total = 0;
+
+// addValueBtn.addEventListener('click', function() {
+//   const value = Number(valueInputRef.value);
+//   numbers.push(value);
+//   valueInputRef.value = '';
+// });
+
+// calcBtn.addEventListener('click', function() {
+//   for (const number of numbers) {
+//     total += number;
+//   }
+//   console.log('Общая сумма чисел равна: ', total);
+// });
+
+// Вариант 2
+const numbers = [];
+let input;
+let total = 0;
+
+while (true) {
+  let input = prompt('Введите число');
+
+  if (input === null) {
+    break;
+  }
+
+  input = Number(input);
+
+  const notANamber = Number.isNaN(input);
+
+  if (notANamber) {
+    alert(`Это не число, введите число`);
+    continue;
+  }
+
+  numbers.push(input);
+
+  for (const number of numbers) {
+    total += number;
+  }
+}
+
+alert(`Общая сумма чисел равна ${total}`);
